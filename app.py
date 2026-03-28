10133731516fa2e6274adf5cc5b5bdaf62bd083a
sudancommunityingreaterlansing <sudancommunityingreaterlansing@gmail.com>
2026-03-28 00:31:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template, request, redirect
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-from datetime import datetime
+#from flask import Flask, render_template, request, redirect
+#import gspread
+#from oauth2client.service_account import ServiceAccountCredentials
+#from datetime import datetime
 
-app = Flask(__name__)
+import os
+import json
+from oauth2client.service_account import ServiceAccountCredentials
+
+creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+
+
+#app = Flask(__name__)
 
 # Google Sheets setup
 scope = [
@@ -11,8 +18,11 @@ scope = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
+
+#creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+#client = gspread.authorize(creds)
 
 sheet = client.open("SudaneseMembership").sheet1  # must match your sheet name
 
